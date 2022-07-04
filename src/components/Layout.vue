c2bcb9b90b169cdfa4e4789264206c5ec7a05692
jogiter <jogiter.g@gmail.com>
2022-07-04 18:46:20 +0800

    feat: use css3 variable to update theme

---
+++ src/components/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <view :style="style">
+    <slot></slot>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'BellLayout',
+  setup() {
+    const style = ref()
+
+    setTimeout(() => {
+      style.value = '--blue: red;'
+    }, 2000)
+
+    return {
+      style,
+    }
+  },
+}
+</script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,44 @@
 <template>
-  <view class="index">
+  <Layout class="index">
     <Counter />
-  </view>
+    <view>
+      <view class="container">
+        <text>Welcome to Shanghai!</text>
+        <text>
+          Shanghai is one of the four direct-administered municipalities of the
+          People's Republic of China.
+        </text>
+        <text>
+          Shanghai is one of the four direct-administered municipalities of the
+          People's Republic of China.
+        </text>
+        <view>
+          <button>Yes</button>
+          <button>No</button>
+        </view>
+      </view>
+      <br />
+
+      <button type="button" @tap="getProperty">
+        使用 JavaScript 来获取 CSS 变量
+      </button>
+      <button type="button" @tap="setProperty">
+        使用 JavaScript 来更改 CSS 变量
+      </button>
+    </view>
+  </Layout>
 </template>
 
 <script lang="ts">
 import './index.css'
+import Layout from '../../components/Layout.vue'
 import Counter from '../../components/Counter.vue'
 
 export default {
   name: 'IndexPage',
   components: {
     Counter,
+    Layout,
   },
   setup() {
     console.log('asf')
